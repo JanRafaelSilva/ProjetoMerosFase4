26c05ea3019a3495be65c2908046d4201bb81666
JanRafaelSilva <jansilvavieira16@gmail.com>
2026-04-01 10:55:49 -0300

    a

--- Assets/Script/Barco/Rede.cs
+++ Assets/Script/Barco/Rede.cs
@@ -5,24 +5,24 @@ public class Rede : MonoBehaviour
     Rigidbody2D rigid;
     GameObject Player;
     float gravity = 10f;
-    float angle = 45;
-    public float forceY = 20f;
-    public float speedX = 10f; 
-    public float velocityI;
+    public float rot;
+    public float angleI = 90f;
+    float range;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>(); 
     }
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
-        float rangeX = Vector3.Distance(this.transform.position, Player.transform.position);
-        float rot = Mathf.Atan2(-transform.position.y, -transform.position.x) * Mathf.Rad2Deg;
-        Debug.Log(rot);
+        Player = GameObject.FindGameObjectWithTag("Player");  
+        Vector2 direction = new Vector2(Player.transform.position.x - transform.position.x,Player.transform.position.y - transform.position.y);
+        rot = (Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg) + angleI;     
     }
     void FixedUpdate()
     {
-        rigid.AddForce(-Vector3.up * gravity, ForceMode2D.Force);
+    }
+}
+//rigid.AddForce(-Vector3.up * gravity, ForceMode2D.Force);
        // rigid.linearVelocity = new Vector2(speedX * Time.fixedDeltaTime, rigid.linearVelocity.y);
         // 1 VEZ
         //rigid.AddForce(Vector3.right * speedX, ForceMode2D.Force);
@@ -31,5 +31,3 @@ public class Rede : MonoBehaviour
         //rb = Mathf.Sqrt((rangeX * gravity)); 
         //rigid.linearVelocityX = speedX * Time.fixedDeltaTime;
         //float rangeMax = Mathf.Pow(rigid)
-    }
-}
