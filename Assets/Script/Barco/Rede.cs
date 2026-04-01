f642592b7c353e10f67befe79d798b3ab0f55225
JanRafaelSilva <jansilvavieira16@gmail.com>
2026-04-01 10:03:02 -0300

    física inicial

--- Assets/Script/Barco/Rede.cs
+++ Assets/Script/Barco/Rede.cs
@@ -6,7 +6,7 @@ public class Rede : MonoBehaviour
     GameObject Player;
     float gravity = 10f;
     float angle = 45;
-    public float forceY = 5f;
+    public float forceY = 20f;
     public float speedX = 10f; 
     public float velocityI;
     void Awake()
@@ -16,14 +16,16 @@ public class Rede : MonoBehaviour
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        float rangeX = Vector3.Distance(this.transform.position, Player.transform.position);
+        float rot = Mathf.Atan2(-transform.position.y, -transform.position.x) * Mathf.Rad2Deg;
+        Debug.Log(rot);
     }
     void FixedUpdate()
     {
         rigid.AddForce(-Vector3.up * gravity, ForceMode2D.Force);
-        
+       // rigid.linearVelocity = new Vector2(speedX * Time.fixedDeltaTime, rigid.linearVelocity.y);
         // 1 VEZ
-        //rigid.AddForce(Vector3.up * forceY);
-       // rigid.linearVelocityX = speedX * Time.fixedDeltaTime;
+        //rigid.AddForce(Vector3.right * speedX, ForceMode2D.Force);
 
         //float rangeX = Vector3.Distance(this.transform.position, Player.transform.position);
         //rb = Mathf.Sqrt((rangeX * gravity)); 
