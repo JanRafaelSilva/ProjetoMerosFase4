100841ce87cda9f7ab910668645f50e4addd2b71
Jan <jansilvavieira16@gmail.com>
2026-04-23 10:41:31 -0300

    Anzol feito

--- Assets/Script/Barco/Anzol.cs
+++ Assets/Script/Barco/Anzol.cs
@@ -2,52 +2,69 @@ using UnityEngine;
 
 public class Anzol : MonoBehaviour
 {
-    public GameObject Meros;
-    public GameObject Barco;
+    public Transform Barco;
+    public Transform Mero;
     Vector3 direction;
-    public float speed = 10f;
-    Rigidbody2D rb;
-    public bool pescou = false;
+    private float speed = 8f;
+    public Rigidbody2D rb;
+    public bool 
+    pescou = false,
+    puxando = false;
     public float timerD;
-    void Awake()
+    public void PlayerScene(Transform Mero, Transform Barco)
     {
-        rb = GetComponentInParent<Rigidbody2D>();
+        // Posições do Mero e do Barco
+        this.Mero = Mero;
+        this.Barco = Barco;
     }
-    void Start()
+    public void Start()
     {
-        Meros = GameObject.FindGameObjectWithTag("Player");
-        Pesca(Meros);
+        // Alvo Mero
+        Pesca(Mero);
     }
+    
     void OnCollisionEnter2D(Collision2D col)
     {
+        // Mero é acertado, Mero vai ser puxado.
         if (col.gameObject.CompareTag("Player"))
         {
             col.transform.SetParent(transform);
             pescou = true;
         }
-        if (col.gameObject.CompareTag("Barco"))
+        // Pesca concluída.
+        if (col.gameObject.CompareTag("Barco") && puxando)
         {
             Destroy(gameObject,1f);
         }
+        // Atingiu uma pedra.
+        if(col.gameObject.layer == 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Pesca(Transform move)
+    { 
+        //direção do movimento
+        Vector2 Gobject = new Vector2(move.position.x, move.position.y);
+        direction = (Gobject - (Vector2)transform.position).normalized;
+        rb.linearVelocity = new Vector2(direction.x * speed, direction.y * speed);
     }
     void Update()
     {
+        //tempo de vida
         timerD += Time.deltaTime;
+        //direção - Barco
         if(pescou == true)
         {
             Pesca(Barco);
             pescou = false;
+            puxando = true;
         }
+        // Fim
         if (timerD >= 10f) 
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
 
-    }
-    public void Pesca(GameObject move)
-    { 
-        Vector2 Gobject = new Vector2(move.transform.position.x, move.transform.position.y);
-        direction = (Gobject - (Vector2)transform.position).normalized;
-        rb.linearVelocity = new Vector2(direction.x * speed, direction.y * speed);
     }
 }
--- Assets/Script/Barco/BoatAnzol.cs
+++ Assets/Script/Barco/BoatAnzol.cs
@@ -1,16 +1,35 @@
+using System.IO;
 using UnityEngine;
 
 public class BoatAnzol : MonoBehaviour
 {
     public float timer, timerMax;
+    public Anzol anzol;
+    BoatAnzol my;
     public GameObject Anzol;
+    public Transform Mero;
+    public void Awake()
+    {
+        anzol = Anzol.GetComponent<Anzol>();
+        my = GetComponent<BoatAnzol>();
+    }
     private void Update()
     {
         timer += Time.deltaTime;
         if(timer >= timerMax)
         {
-            Instantiate(Anzol, new Vector2(transform.position.x, transform.position.y - 5), Quaternion.identity);
-            timer = 0;
+            ins();
         }
+        if(Mero.position.x <= transform.position.x)
+        {
+            ins();
+            my.enabled = false;
+        }
+    }
+    void ins()
+    {
+        Instantiate(Anzol, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        anzol.PlayerScene(Mero, transform);
+        timer = 0;
     }
 }
