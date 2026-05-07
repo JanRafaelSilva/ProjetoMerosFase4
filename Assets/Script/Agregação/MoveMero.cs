062ac5271769c20999f943807926d89d2e67c414
Jan <jansilvavieira16@gmail.com>
2026-05-07 11:20:52 -0300

    Melhora na movimentação da IA
    Transformação do ambiente, para mais parecido com uma agregação.

--- Assets/Script/Agregação/MoveMero.cs
+++ Assets/Script/Agregação/MoveMero.cs
@@ -1,7 +1,9 @@
 using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
+[ RequireComponent (typeof( NavMeshAgent ))]
 public class MoveMero : MonoBehaviour
 {
     public Vector2 direction;
@@ -13,11 +15,12 @@ public class MoveMero : MonoBehaviour
     public float OffsetRadius;
     public float moveMax = 3f;
     public float moveMin = -3f;
-    public Vector3 velocity;
     public float stop;
-
+    public float acceleration;
+    public bool EndMap;
 
     public float timeRandom, timeMin, timeMax;
+    public float EndMapTime;
     public float moveMin_X,moveMax_X,moveMinY, moveMaxY;
     //Ponto para o qual o personagem irá se mover
     //Variável NavMeshAgent Para configurar A movimentação do personagem
@@ -35,22 +38,34 @@ public class MoveMero : MonoBehaviour
         //Variaveis setadas como False para Não utilizar os eixos Y Baseado em 3 dimensões
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        agent.acceleration = acceleration;
         agent.speed = speed;
-        agent.acceleration = 2f;
     }
     private void Update()
     {
-        //HandleFlip();
+
         timeRandom -= Time.deltaTime;
         if(timeRandom <= 0f)
         {
             DirectionRandom();
-            
+        }
+        if(agent.velocity.magnitude < 0.15f && EndMap)
+        {
+           direction *= -1;
+           EndMap = false;
+        }
+        else
+        {
+            EndMapTime += Time.deltaTime;
+            if(EndMapTime >= 4f)
+            {
+                EndMap = true;
+                EndMapTime = 0f;
+            }
         }
     } 
     private void FixedUpdate()
     {
-        //Faz o personagem se locomover pelo cenario até o point
         Movimento();
 
     }
@@ -59,44 +74,32 @@ public class MoveMero : MonoBehaviour
         timeRandom = Random.Range(timeMin, timeMax);
         direction.x = Random.Range(moveMin_X, moveMax_X);
         direction.y = Random.Range(moveMinY, moveMaxY);
+        if (direction.x > 0f) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (direction.x < 0f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
     public void Movimento()
     {
         agent.SetDestination(new Vector3(transform.position.x + direction.x, transform.position.y + direction.y, 0f));
-        agent.angularSpeed = direction.y * rotacaoMax;
-
-    }
-     void HandleFlip()
-    {
-        float escalaAntiga = transform.localScale.x;
-        if (direction.x > 0.1f) 
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (direction.x < -0.1f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        if (transform.localScale.x != escalaAntiga)
-        {
-            Vector3 rot = transform.eulerAngles;
-            float z = rot.z;
-            if (z > 180) z -= 360;
-
-            transform.rotation = Quaternion.Euler(0, 0, -z);
-
-            velRotacao = 0;
-        }
-
-    }
-    public void OnCollisionEnter2D(Collision2D col)
-    {
-        DirectionRandom();
     }
 }
-/*
-  direction.x = transform.position.x >= meio.transform.localPosition.x ? moveMinX : moveMaxX;
-  direction.x = transform.position.x <= meio.transform.localPosition.x ? moveMaxX : moveMinX;
-  direction.y = transform.position.y >= meio.transform.localPosition.y ? moveMinY : moveMaxY;
-  direction.y = transform.position.y <= meio.transform.localPosition.y ? moveMaxY : moveMinY;        */
+// utilizar autoBraking: Should the agent brake automatically to avoid overshooting the destination point?
+// pós autoRepath: Should the agent attempt to acquire a new path if the existing path becomes invalid?
+// talvez usar avoidancePriority: The avoidance priority level.
+
+
+//agent.velocity = rb.linearVelocity;
+        //rb.AddForce(direction * agent.speed);
+        //rb.linearVelocity = new Vector2(Mathf.Clamp(rb.linearVelocity.x, moveMin, moveMax),Mathf.Clamp(rb.linearVelocity.y, moveMin, move
+        //	Maximum turning speed in (deg/s) while following a path.
+      //  agent.angularSpeed = direction.y * rotacaoMax;
+      //	The maximum acceleration of an agent as it follows a path, given in units / sec^2.
+
+      //NavMesh.FindClosestEdge
+
+      //Inicio o projeto em junho
